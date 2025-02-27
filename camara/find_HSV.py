d93acfcd8eea7a045a6294e4053dbfd1ec4c435b
sunrise7889 <a0986009353@gmail.com>
2025-02-27 18:14:35 +0800

    用測試圖檔實驗找HSV，但只有一組數字沒有upper跟downer

--- camara/find_HSV.py
+++ camara/find_HSV.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-image=cv2.imread('hsv.png')
+image=cv2.imread('D:\code\YunTech_code\camara\hsv.png')
 HSV=cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
 def getpos(event,x,y,flags,param):
     if event==cv2.EVENT_LBUTTONDOWN: #定义一个鼠标左键按下去的事件
@@ -12,4 +12,3 @@ cv2.imshow("imageHSV",HSV)
 cv2.imshow('image',image)
 cv2.setMouseCallback("imageHSV",getpos)
 cv2.waitKey(0)
-
