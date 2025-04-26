07067f607d6e116df4864d02f78c2235c2b7bdde
sunrise7889 <a0986009353@gmail.com>
2025-04-26 16:57:13 +0800

    加入黑點模擬手臂

--- camera/new_merge.py
+++ camera/new_merge.py
@@ -19,6 +19,7 @@ import joblib
 pred = joblib.load("svm_model_no_scaler.pkl")
 
 # === 參數設定 ===
+arm_pos = [300, 150] #手臂位置模擬
 intersection_points = [] #交點儲存
 grid_cols = 4  # 橫向幾格（x方向）
 grid_rows = 3  # 縱向幾格（y方向）
@@ -278,7 +279,12 @@ try:
                     y = defense_top + row * cell_h
                     intersection_points.append((x, y))
                     cv2.circle(warped, (x, y), 3, (0, 255, 255), -1)  # 黃色小點交點
-                    
+        
+        # == 模擬手臂 ===
+        cv2.circle(warped, tuple(arm_pos), 8, (0, 0, 0), -1)
+        cv2.putText(warped, f"Arm ({arm_pos[0]}, {arm_pos[1]})", (arm_pos[0]+10, arm_pos[1]),
+            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1) 
+        ㄏ
         if cx_g is not None and cy_g is not None:
             puck_pos = np.array([cx_g, cy_g])
             closest = min(intersection_points, key=lambda pt: np.linalg.norm(puck_pos - np.array(pt)))
